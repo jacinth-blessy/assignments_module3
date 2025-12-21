b20123d53f9fc91a85e8b9c28659cc7bbe790ad3
jacinth-blessy <jacinthmblessy2024@gmail.com>
2025-12-21 17:57:13 +0530

    added todocard component

--- react_styling_lists/src/App.jsx
+++ react_styling_lists/src/App.jsx
@@ -1,12 +1,23 @@
+import React, { useState } from "react";
 import StatusComponent from "./StatusComponent";
 import ConditionalStyling from "./ConditionalStyling";
-
+import TodosList from "./TodosList";
 
 function App() {
+  const [showTodos, setShowTodos] = useState(true);
+
   return (
     <div>
       <StatusComponent />
       <ConditionalStyling />
+
+      <div style={{ marginTop: 20 }}>
+        <button onClick={() => setShowTodos((s) => !s)}>
+          {showTodos ? "Unmount Todos" : "Mount Todos"}
+        </button>
+      </div>
+
+      {showTodos && <TodosList />}
     </div>
   );
 }
